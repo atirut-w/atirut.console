b3f6627fa4a4766e8e188d82b38b33a70c511745
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2023-06-19 18:18:32 +0700

    Console controller stub

--- console.gd
+++ console.gd
@@ -1,1 +1,13 @@
 extends Control
+
+
+func _ready() -> void:
+	visible = false
+	if InputMap.has_action("console_toggle") == false:
+		push_error("Action `console_toggle` does not exist. Please set it up in input settings.")
+		process_mode = Node.PROCESS_MODE_DISABLED
+
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("console_toggle"):
+		visible = !visible
